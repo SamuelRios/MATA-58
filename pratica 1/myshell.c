90c6b62066cf01c49cad77baaf0c774e2acabc97
Samuel Rios <mucarioss@gmail.com>
2022-05-03 20:25:01 -0700

    intial commit pratica1

---
+++ pratica 1/myshell.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(){
+
+    char commandl[] = "";
+
+    if(commandl != NULL){
+
+        printf("myshell> ");
+        fflush(stdout);
+
+        while(fgets(commandl, 4097, stdin) != NULL){
+
+            printf("%s", commandl);
+            printf("%d\n", (int)strlen(commandl));  
+
+            char* words[100];
+            int wcount = 0;
+            words[wcount] = strtok(commandl, " ");
+            while(wcount < 100 && words[wcount]){
+                // printf("%s\n", words[wcount] );
+                // words[wcount] = strtok(NULL, " ");
+                // wcount++;
+            }
+            printf("myshell> ");
+            fflush(stdout);
+        }
+        free(commandl);
+    }
+   
+    
+    
+    sleep(5);
+}
